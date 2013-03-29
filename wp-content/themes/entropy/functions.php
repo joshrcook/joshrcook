57c99928e224a477107b63b289844ee9ae5ecf9c
Mingei Website <jyork@mingei.org>
2013-03-29 00:08:57 -0400

    Added support for menus

--- wp-content/themes/entropy/functions.php
+++ wp-content/themes/entropy/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function add_menu_support() {
+    
+    add_theme_support('menus');
+    
+}
+
+add_action('after_setup_theme', 'add_menu_support');
+
 /**
  * Registers the Main Navigation Menu so that it can be called in a template
  */
