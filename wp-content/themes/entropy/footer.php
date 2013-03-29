224f202e34bee02716686e3cd79d361d351c0777
Mingei Website <jyork@mingei.org>
2013-03-29 00:04:31 -0400

    Changes to footer

--- wp-content/themes/entropy/footer.php
+++ wp-content/themes/entropy/footer.php
@@ -4,7 +4,8 @@
 				<li><?php previous_posts_link('&larr; Previous Page', 0) ?></li>
 				<li><?php next_posts_link('Next Page &rarr;', 0); ?></li>
 			</ul>
-			<p>&copy; <?php the_time('Y') ?> <?php bloginfo('name'); ?>. All Rights Reserved. <a href="http://timothy-long.com/entropy">Entropy Wordpress Theme</a> by <a href="http://timothy-long.com">Timothy Long</a></p>
+			<p><a href="<?php get_permalink(get_page_by_title('Contact')); ?>">Contact Me.</a><br />
+                            &copy; <?php the_time('Y') ?>. All Rights Reserved. <a href="http://timothy-long.com/entropy">Entropy Wordpress Theme</a> by <a href="http://timothy-long.com">Timothy Long</a></p>
 		</div>
 	</footer>
 </body>
