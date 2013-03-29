d4ce4afae8ad47da15bb8216f13708be04d30b28
Mingei Website <jyork@mingei.org>
2013-03-28 23:48:35 -0400

    Changes to page template

--- wp-content/themes/entropy/page.php
+++ wp-content/themes/entropy/page.php
@@ -2,7 +2,7 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<article>
 		<div class="container">
-			<h2><?php the_title(); ?></h2>
+			<h1><?php the_title(); ?></h1>
 			<?php the_content(); ?> 
 		</div>
 	</article>
